5b0b2fe37e18e88b339da0be10a3c7e85320e34a
William Bojczuk <williambojczuk@gmail.com>
2024-11-10 16:28:04 -0500

    udated

--- src/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo.tsx
+++ src/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo.tsx
@@ -20,11 +20,16 @@ export default function ContactInfo() {
               <a href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUM}`}>{process.env.NEXT_PUBLIC_PHONE_NUM_FORMATTED}</a>
             </div>
 
-            {/* <div className={`${styles.contentWrapper} ${styles.facebook}`}>
-            <a href={`https://www.facebook.com/BeMoreandLive`} target="_blank">
+            <div className={`${styles.contentWrapper} ${styles.facebook}`}>
+            <a href={`https://www.facebook.com/profile.php?id=61568233932850`} target="_blank">
               <h5><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"></path></svg> Facebook</h5>
             </a>
-            </div> */}
+            </div>
+            <div className={`${styles.contentWrapper} ${styles.facebook}`}>
+            <a href={`https://www.instagram.com/essentialdnatesting/`} target="_blank">
+              <h5><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M13 10a3 3 0 1 1-6 0c0-.171.018-.338.049-.5H6v3.997c0 .278.225.503.503.503h6.995a.503.503 0 0 0 .502-.503V9.5h-1.049c.031.162.049.329.049.5m-3 2a2 2 0 1 0-.001-4.001A2 2 0 0 0 10 12m2.4-4.1h1.199a.301.301 0 0 0 .301-.3V6.401a.301.301 0 0 0-.301-.301H12.4a.301.301 0 0 0-.301.301V7.6c.001.165.136.3.301.3M10 .4A9.6 9.6 0 0 0 .4 10a9.6 9.6 0 0 0 9.6 9.6a9.6 9.6 0 0 0 9.6-9.6A9.6 9.6 0 0 0 10 .4m5 13.489C15 14.5 14.5 15 13.889 15H6.111C5.5 15 5 14.5 5 13.889V6.111C5 5.5 5.5 5 6.111 5h7.778C14.5 5 15 5.5 15 6.111z"></path></svg> Instagram</h5>
+            </a>
+            </div>
 
             <img src="/img/contact.webp" alt="image of dna strand" className="bg-img" />
             <div style={{backgroundColor: "rgba(0,0,0,.56)"}} className="shader"></div>
